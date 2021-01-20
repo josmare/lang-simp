10a638cd2cb880cd408c35587358890257ad6f0c
Josmar <josmar.regalado@gmail.com>
2021-01-20 18:00:27 +0200

    Mock chart

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 
 @application.route('/')
-def hello_mundo():
-    return 'Que onda, banda.'
+def lang_simp():
+    datos = {'date': [1, 2, 3, 4, 5],
+             'original': [13, 20, 200, 100, 5, 100],
+             'simpli': [1, 20, 30, 43, 51]}
+
+    return render_template('index.html', datos=datos)
