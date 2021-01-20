368f18be1b37f92548f2e81fcae786e475296f2e
Josmar <josmar.regalado@gmail.com>
2021-01-20 22:10:29 +0200

    Plotting functionality
    input a function in numpy format

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import numpy as np
 import sys
 
@@ -6,10 +6,14 @@ application = Flask(__name__)
 
 @application.route('/', methods=['GET', 'POST'])
 def lang_simp():
-    formula_str = 'np.exp(-t) * np.cos(2*np.pi*t)'
+    if request.method == 'POST':
+        formula_str = request.form['formula']
+    else:
+        formula_str = 'np.exp(-t) * np.cos(2*np.pi*t)'
+    print(formula_str, file=sys.stderr)
     t = np.arange(0.0, 5.0, 0.01)
     f1 = list(eval(formula_str))
     datos1 = {'y': f1}
 
-    return render_template('index.html', datos=datos1)
+    return render_template('index.html', datos=datos1, formula_str=formula_str)
 
